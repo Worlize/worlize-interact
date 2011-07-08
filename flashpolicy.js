72b36d2597386a9038bc88d632c9d05a105cd3a7
Brian McKelvey <brian@worlize.com>
2011-07-07 22:46:11 -0700

    Adding --debug flag to flashpolicy.js to support local development.

--- flashpolicy.js
+++ flashpolicy.js
@@ -3,17 +3,39 @@
 var net = require('net'),
     sys = require('sys');
 
-policy = '<?xml version="1.0"?>\n' +
-         '<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n' +
-         '<cross-domain-policy>\n' +
-         '<allow-access-from domain="*.worlize.com" to-ports="80,443"/>\n' +
-         '<allow-access-from domain="localhost" to-ports="80,443"/>\n' +    
-         '</cross-domain-policy>\n';
+
+var args = { /* defaults */
+    devel: false
+};
+
+/* Parse command line options */
+var pattern = /^--(.*?)(?:=(.*))?$/;
+process.argv.forEach(function(value) {
+    var match = pattern.exec(value);
+    if (match) {
+        args[match[1]] = match[2] ? match[2] : true;
+    }
+});
+
+
+policy  = '<?xml version="1.0"?>\n';
+policy += '<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n';
+policy += '<cross-domain-policy>\n';
+policy += '<allow-access-from domain="*.worlize.com" to-ports="80,443"/>\n';
+if (args.devel) {
+    policy += '<allow-access-from domain="*.worlize.local" to-ports="80,443"/>\n';
+    policy += '<allow-access-from domain="localhost" to-ports="80,443"/>\n';
+}
+policy += '</cross-domain-policy>\n';
 
 net.createServer(function(socket){
-        socket.write(policy);
+    socket.write(policy);
 	socket.end();
 	console.log("Provided response to " + socket.remoteAddress);
 }).listen(843);
 
 console.log("Ready to accept connections on port 843.")
+
+if (args.devel) {
+    console.log("Development mode enabled.  Allowing access from *.worlize.local and localhost");
+}
